eaf98219abf7d5ad59352701419493feb226f8d6
panenmin <568014904@qq.com>
2020-05-03 15:25:41 +0800

    simple factory demo

---
+++ Operation.java
@@ -0,0 +1,18 @@
+﻿public abstract class Operation {       // 抽象类
+    private double numberA = 0;
+    private double numberB = 0;
+    public double getNumberA () {
+        return numberA;
+    }
+    public void setNumberA (double value) {
+        numberA = value;
+    }
+    public double getNumberB () {
+        return numberB;
+    }
+    public void setNumberB (double value) {
+        numberB = value;
+    }
+    public abstract double getResult();        // 定义了抽象方法  必须定义抽象类
+}
+
---
+++ Test.java
@@ -0,0 +1,11 @@
+﻿
+public class Test {
+    public static void main(String [] args) {
+        Operation oper;
+        oper = OperationFactory.createOperate("+");
+        oper.setNumberA(1);
+        oper.setNumberB(2);
+        double result = oper.getResult();
+        System.out.println(result);
+    }
+}
